422e419e461e468c1614fa179a04cf93354b5d3f
Mark Dickinson <monoposon@comcast.net>
2016-07-13 08:51:57 -0600

    Initial

---
+++ README.md
@@ -0,0 +1,6 @@
+#Intro to higher order functions
+
+
+Below you'll find multiple functions that are invoked in odd and mysterious ways. Using what you know now about data structures and higher order functions, create a new repository with a single JavaScript file.
+
+Write the function definitions at the bottom of the file. At the top, write statements that prove your functions work.
---
+++ functions.js
@@ -0,0 +1,25 @@
+// #1.
+
+console.log('Running any expecting: true!');
+console.log(any([8,9,10,11], greaterThan10));
+
+// #2.
+
+console.log('Running surprise()() expecting: surprise!');
+console.log(suprise()());
+
+// #3.
+
+console.log('Running filter expecting: [1, 3]');
+console.log(filter([1, 2, 3, 4], onlyOdd));
+
+// #4.
+
+console.log('Running sumTwoNumbers(3)(9) expecting: 12');
+console.log(sumTwoNumbers(3)(9));
+
+// #1.
+
+function any(array, callback) {
+  return callback()
+}
